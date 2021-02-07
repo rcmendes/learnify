ae2529a3e4b653543b7c7127e1dafea372b57f62
Rodrigo Mendes <rcmendes80@gmail.com>
2021-02-07 17:54:38 -0500

    added loading from the backend.

--- frontend/pages/game/quizzes/[id].tsx
+++ frontend/pages/game/quizzes/[id].tsx
@@ -1,9 +1,15 @@
 import { GetStaticPaths, GetStaticProps } from "next"
+import styled from "styled-components"
+
+type MediaInfo = {
+    id: string;
+    uri: string;
+}
 
 type Quiz = {
-    id: string;
-    images: string[];
-    audio: string;
+    question: string;
+    images: MediaInfo[];
+    audio: MediaInfo;
 
 }
 
@@ -13,28 +19,34 @@ type Game = {
     quizzes: string[];
 }
 
+
+
+
 export default function QuizBoard({ quiz }: { quiz: Quiz }) {
     return (
         <div>
             <div>
-                Quiz: {quiz.id}
+                Quiz: {quiz.question}
             </div>
             <div>
-                Sound: <audio src={quiz.audio}></audio>
-                <small>{quiz.audio}</small>
+                Sound:
+                <audio controls autoPlay>
+                    <source src={quiz.audio.uri} type="audio/ogg" />
+                </audio>
+                <small>{quiz.audio.id}</small>
             </div>
-            <div>
+            <ImageGrid>
                 {
                     quiz.images.map(img => (
-                        <div key={img}>
+                        <ImageBox key={img.id}>
                             <div>
-                                <img src={img} alt="description of the image" />
+                                <img src={img.uri} alt="description of the image" />
                             </div>
-                            <small>{img}</small>
-                        </div>
+                            <small>{img.id}</small>
+                        </ImageBox>
                     ))
                 }
-            </div>
+            </ImageGrid>
         </div>
     )
 }
@@ -42,15 +54,33 @@ export default function QuizBoard({ quiz }: { quiz: Quiz }) {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
     const quizID = params.id;
 
-    const quiz: Quiz = {
-        id: "a",
-        audio: "a/audio",
-        images: [
-            "a/images",
-            "b/images",
-            "c/images",
-        ]
-    }
+    const response = await fetch("http://localhost:8080/game/44e420ad-c5fd-4d26-9ed5-2838f2450147");
+    const quiz = await response.json();
+    // const quiz: Quiz = {
+    //     id: "44e420ad-c5fd-4d26-9ed5-2838f2450147",
+    //     images: [
+    //         {
+    //             id: "44e420ad-c5fd-4d26-9ed5-2838f2450147",
+    //             uri: "http://localhost:8080/quizzes/44e420ad-c5fd-4d26-9ed5-2838f2450147/image"
+    //         },
+    //         {
+    //             id: "93ac3e8e-e82b-4a4f-adf8-f09d4857b0e0",
+    //             uri: "http://localhost:8080/quizzes/93ac3e8e-e82b-4a4f-adf8-f09d4857b0e0/image"
+    //         },
+    //         {
+    //             id: "32ed9040-1c14-4875-9ab3-c504dc7a962c",
+    //             uri: "http://localhost:8080/quizzes/32ed9040-1c14-4875-9ab3-c504dc7a962c/image"
+    //         },
+    //         {
+    //             id: "5509e717-a4de-4457-b3b9-5b1676e591e4",
+    //             uri: "http://localhost:8080/quizzes/5509e717-a4de-4457-b3b9-5b1676e591e4/image"
+    //         }
+    //     ],
+    //     audio: {
+    //         id: "44e420ad-c5fd-4d26-9ed5-2838f2450147",
+    //         uri: "http://localhost:8080/quizzes/44e420ad-c5fd-4d26-9ed5-2838f2450147/audio"
+    //     }
+    // }
 
     return {
         props: { quiz },
@@ -60,6 +90,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
+
     const game: Game = {
         id: "1000",
         quizzes: ["a", "b", "c"],
@@ -77,3 +108,17 @@ export const getStaticPaths: GetStaticPaths = async () => {
         fallback: false,
     }
 }
+
+const ImageGrid = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+`
+
+const ImageBox = styled.div`
+    ::hover {
+        cursor: pointer;
+    };
+
+    border: 1 px solid;
+`
