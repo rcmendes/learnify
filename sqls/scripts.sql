2f39c61968a0f2736df39c827b8b5178e4db1c38
Rodrigo Mendes <rcmendes80@gmail.com>
2021-03-02 07:18:10 -0500

    added player schema into database.

--- sqls/scripts.sql
+++ sqls/scripts.sql
@@ -7,9 +7,15 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 -- DROP TABLE public.quizzes;
 
+-- public.quizzes definition
+
+-- Drop table
+
+-- DROP TABLE public.quizzes;
+
 CREATE TABLE public.quizzes (
-	id uuid NOT NULL DEFAULT uuid_generate_v4(),
-	category varchar(40) NOT NULL,
+	id uuid NOT NULL,
+	category_id uuid NOT NULL,
 	palavra varchar(60) NOT NULL,
 	mot varchar(60) NOT NULL,
 	image_filename varchar(255) NOT NULL,
@@ -19,29 +25,36 @@ CREATE TABLE public.quizzes (
 	CONSTRAINT quizzes_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE public.game_quiz (
-	game_id uuid NOT NULL,
-	quiz_id uuid not null,
+CREATE TABLE public.categories (
+	id uuid NOT NULL,
+	name varchar(40) not null unique,
+	description varchar(255) null,
+	created_at timestamptz(0) NOT NULL DEFAULT now(),
 	updated_at timestamptz(0) NOT NULL DEFAULT now(),
-	success bool null,
-	CONSTRAINT game_quizzes_pk PRIMARY KEY (game_id, quiz_id)
+	CONSTRAINT categories_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE public.game (
+CREATE TABLE public.games (
 	id uuid NOT NULL,
 	player_id uuid NOT NULL,
-	status char NOT null default 'C',
+	status smallint NOT null default 1,
 	created_at timestamptz(0) NOT NULL DEFAULT now(),
 	updated_at timestamptz(0) NOT NULL DEFAULT now(),
 	CONSTRAINT game_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE public.player (
-	id uuid NOT NULL,
-	name varchar(100) NOT NULL,
-	status char NOT null default 'C',
-	created_at timestamptz(0) NOT NULL DEFAULT now(),
+CREATE TABLE public.game_quizzes (
+	game_id uuid NOT NULL,
+	quiz_id uuid not null,
 	updated_at timestamptz(0) NOT NULL DEFAULT now(),
+	status smallint not null default 1,
+	CONSTRAINT game_quizzes_pk PRIMARY KEY (game_id, quiz_id)
+);
+
+CREATE TABLE public.players (
+	id uuid NOT NULL,
+	name varchar(40) NOT NULL,
+	created_at timestamptz(0) NOT NULL DEFAULT now(),
 	CONSTRAINT player_pk PRIMARY KEY (id)
 );
 
@@ -51,7 +64,7 @@ ALTER TABLE public.quizzes ADD updated_at timestamptz(0) NOT NULL DEFAULT now();
 
 
 
-insert into player(id, name) values (uuid_generate_v4(), 'Guga')
+insert into players(id, name) values (uuid_generate_v4(), 'Guga')
 
 
 select id, tag, palavra, url, mot from quizzes q where q.tag ='animais' order by q.tag;
@@ -60,3 +73,5 @@ select id, tag, palavra, url from quizzes q where q.tag ='animais' order by q.ta
 
 
 
+
+select * from players where id like '20f2777e-6bf8-409b-affc-9f7731eea714' as uuid
